6f40ecfff1629206132f40b4a9a1138608e5535c
Krzysztof Ligeska <krzysztof.ligeska.2@aptiv.com>
2023-10-23 17:34:00 +0200

    first cards commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+
+  posts = [
+    { title: 'tree', imageUrl: 'assets/tree.jpeg', userName: 'Michel', content: 'I saw this once' },
+    { title: 'hike', imageUrl: 'assets/mountain.jpeg', userName: 'Bruno', content: 'I was there' },
+    { title: 'tree', imageUrl: 'assets/biking.jpeg', userName: 'Janine', content: 'I want to ride my bicycle' },
+  ];
 }
